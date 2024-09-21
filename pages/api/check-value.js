06e2e8219f3622c6d982585ee5dfbf0faa22cded
lucas.vernotte <lucas.vernotte@syrlinks.com>
2024-09-21 14:52:25 +0200

    hash

--- pages/api/check-value.js
+++ pages/api/check-value.js
@@ -1,6 +1,7 @@
 // pages/api/check-value.js
 import fs from 'fs';
 import path from 'path';
+import CryptoJS from 'crypto-js';
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
@@ -9,15 +10,14 @@ export default function handler(req, res) {
     // Lire le fichier du serveur contenant la valeur de comparaison
     const filePath = path.resolve(process.cwd(), 'data/secret-value.txt');
     const storedValue = fs.readFileSync(filePath, 'utf-8').trim();
-    // const hash = CryptoJS.SHA256(value).toString();
+    const hashedValue = CryptoJS.SHA256(value).toString(CryptoJS.enc.Hex);
     // Comparer l'entrée utilisateur avec la valeur stockée
-    if (value === storedValue) {
-      res.status(200).json({ message: 'Valeur correcte !' });
+    if (hashedValue === storedValue) {
+      res.status(200).json({ message: 'Valeur correcte !'+hashedValue});
     } else {
-      res.status(200).json({ message: 'Valeur incorrecte.' });
+      res.status(200).json({ message: 'Valeur incorrecte.'+hashedValue});
     }
   } else {
     res.status(405).json({ message: 'Méthode non autorisée.' });
   }
-  const hash = CryptoJS.SHA256(value).toString();
 }
