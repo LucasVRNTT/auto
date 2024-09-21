7fcb5c221f4a941e21abee194f5354ee2b6fdd14
lucas.vernotte <lucas.vernotte@syrlinks.com>
2024-09-21 15:19:34 +0200

    update checkauth

--- pages/api/check-auth.js
+++ pages/api/check-auth.js
@@ -1,8 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { parse } from 'cookie';
-
-const secret = 'votre_secret';
-
+const filePath = path.resolve(process.cwd(), 'data/secret-value.txt');
+const storedValue = fs.readFileSync(filePath, 'utf-8').trim();
 export default function handler(req, res) {
   const { auth } = parse(req.headers.cookie || '');
 
@@ -11,7 +10,7 @@ export default function handler(req, res) {
   }
 
   try {
-    jwt.verify(auth, secret);
+    jwt.verify(auth, storedValue);
     return res.status(200).json({ authenticated: true });
   } catch (err) {
     return res.status(401).json({ authenticated: false });
