15a43cf6d82d8b5028bf75f434454a91b48b4c2a
lucas.vernotte <lucas.vernotte@syrlinks.com>
2024-09-22 13:04:58 +0200

    ee

--- pages/api/load-html.js
+++ pages/api/load-html.js
@@ -4,8 +4,17 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const filePath = path.resolve(process.cwd(), 'pages/board.html');
-  const htmlContent = fs.readFileSync(filePath, 'utf-8');
-  
-  res.status(200).send(htmlContent);
+  // Spécifiez le chemin du fichier HTML
+  const filePath = path.join(process.cwd(), 'pages', 'board.html'); // Assurez-vous que le fichier existe
+
+  // Lire le contenu du fichier
+  fs.readFile(filePath, 'utf-8', (err, htmlContent) => {
+    if (err) {
+      console.error("Erreur lors de la lecture du fichier HTML:", err);
+      return res.status(500).json({ error: 'Erreur de lecture du fichier HTML' });
+    }
+
+    // Envoyer le contenu HTML dans la réponse
+    res.status(200).send(htmlContent);
+  });
 }
